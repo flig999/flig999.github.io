d3093c38da2273f042374fee9e6913f41a31680b
flig999 <bur17039@byui.edu>
2020-12-14 16:43:17 -0700

    chaning json file

--- Lesson12/js/temples.js
+++ Lesson12/js/temples.js
@@ -1,3 +1,8 @@
+const DallasWeather = "https://api.openweathermap.org/data/2.5/weather?id=4684888&appid=fd74cb44a609831d55dcf1e741ec4e9b";
+const ColumbiaWeather = "https://api.openweathermap.org/data/2.5/weather?id=5790614&appid=fd74cb44a609831d55dcf1e741ec4e9b";
+const FortWeather = "https://api.openweathermap.org/data/2.5/weather?id=4155966&appid=fd74cb44a609831d55dcf1e741ec4e9b";
+const HartWeather = "https://api.openweathermap.org/data/2.5/weather?id=4835797&appid=fd74cb44a609831d55dcf1e741ec4e9b";
+
 const templeURL = 'https://flig999.github.io/Lesson12/temples.json';
 
 // requests (fetches) the json file
@@ -7,9 +12,11 @@ fetch(templeURL)
     })
     .then(function (jsonObject) {
         // store results in an array
+        
         const temples = jsonObject["temples"];
         for (let i = 0; i < temples.length; i++) {
             let card = document.createElement('section');
+            let text = document.createElement('div');
             let temple = document.createElement('h2');
             let address = document.createElement('p');
             let telephone = document.createElement('p');
@@ -19,6 +26,10 @@ fetch(templeURL)
             let ordinance = document.createElement('p');
             let session = document.createElement('p');
             let closure = document.createElement('p');
+            let weather = document.createElement('p');
+            let temp = document.createElement('p');
+            let wind = document.createElement('p');
+            let humid = document.createElement('p');
             let image = document.createElement("img");
             temple.textContent = temples[i].name;
             address.textContent = "Address: " + temples[i].address;
@@ -29,17 +40,34 @@ fetch(templeURL)
             ordinance.textContent = "Ordinance Schedule: " + temples[i].ordinance;
             session.textContent = "Session Schedule: " + temples[i].session;
             closure.textContent = "Closure Schedule: " + temples[i].closure;
+            weather.textContent = "Current Temperature: "
             image.setAttribute('src', 'images/' + temples[i].image);
-            image.setAttribute('class', 'town');
-            card.appendChild(temple);
-            card.appendChild(address);
-            card.appendChild(telephone);
-            card.appendChild(email);
-            card.appendChild(services);
-            card.appendChild(history);
-            card.appendChild(ordinance);
-            card.appendChild(session);
-            card.appendChild(closure);
+            image.setAttribute('class', 'templeimage');
+
+            fetch(temple[i].weather)
+            .then((response) => response.json())
+  
+            .then((jsObject) => {
+                console.log(jsObject);
+                temp.textContent = "Temperature: " + Math.round((((jsObject.main.temp) - 273.15) * 9/5 + 32)*100)/100;
+                humid.textContent = "Humidity: " + jsObject.main.humidity;
+                wind.textContent = "Windspeed: " + jsObject.wind.speed;
+            });
+
+            text.appendChild(temple);
+            text.appendChild(address);
+            text.appendChild(telephone);
+            text.appendChild(email);
+            text.appendChild(services);
+            text.appendChild(history);
+            text.appendChild(ordinance);
+            text.appendChild(session);
+            text.appendChild(closure);
+            weather.appendChild(temp);
+            weather.appendChild(humid);
+            weather.appendChild(wind);
+            text.appendChild(weather);
+            card.appendChild(text);
             card.appendChild(image);
             document.getElementById('temples').appendChild(card);
         }
