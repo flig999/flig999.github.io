11f8c6b90e9dd7cb7164f75a63d7c0c3162ea930
flig999 <bur17039@byui.edu>
2020-12-14 14:08:05 -0700

    again!

--- Lesson12/js/temples.js
+++ Lesson12/js/temples.js
@@ -43,34 +43,3 @@ fetch(templeURL)
             document.querySelector('div.temples').appendChild(card);
         }
     });
-
-
-    // example from lesson9
-    for (let i = 0; i < towns.length; i++) {
-        if ( towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs"){
-        let card = document.createElement('section');
-        let text = document.createElement('div');
-        let h2 = document.createElement('h2');
-        let h6 = document.createElement('h6');
-        let image = document.createElement('img');
-        let founded = document.createElement('p');
-        let population = document.createElement('p');
-        let rain = document.createElement('p');
-        h2.textContent = towns[i].name;
-        h6.textContent = towns[i].motto;
-        image.setAttribute('src', 'images/' + towns[i].photo);
-        image.setAttribute('class', 'town');
-        text.setAttribute('class', 'towntext');
-        founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        population.textContent = 'Population: ' + towns[i].currentPopulation;
-        rain.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-        card.appendChild(text);
-        card.appendChild(image);
-        text.appendChild(h2);
-        text.appendChild(h6);
-        text.appendChild(founded);
-        text.appendChild(population);
-        text.appendChild(rain);
-        document.querySelector('div.indexTowns').appendChild(card);
-    }
-    }
